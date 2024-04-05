002c21af46e3a9f53478a72a4451cb8314c0f5a2
Bunz <66bunz@gmail.com>
2024-04-05 22:51:43 +0200

    commit

--- Esercitazione0/es6.asm
+++ Esercitazione0/es6.asm
@@ -1,8 +1,8 @@
 .data
-    DIM=4
-    wRes: .space 20
+    DIM=5
+    wVett: .space 20
     message_in: .asciiz "Inserire numeri:\n"
-    message_out: .asciiz "Numeri inseriti:\n"
+    message_out: .asciiz "\nNumeri inseriti:\n"
     space: .ascii ";"
 
 .text
@@ -10,25 +10,54 @@
 .ent main
 
 main:
-    # TODO: da completare
     la $t0, wVett
     li $t1, 0
-
-
-    li $v0, 4, 
+    
+    # Stampa il messaggio di input
     la $a0, message_in
+    li $v0, 4
     syscall
 
-
-
-    #  Lettura intero
+ciclo_inserimenti:
+    # Legge un intero
     li $v0, 5
     syscall
+
+    # Salva l'intero nel vettore
     sw $v0, ($t0)
 
+    # Aggiorna l'indirizzo del vettore e il contatore
+    add $t0, $t0, 4
+    add $t1, $t1, 1
 
+    # Controlla se abbiamo letto 5 numeri
+    bne $t1, DIM, ciclo_inserimenti
 
+    # Stampa il messaggio di output
+    li $v0, 4
+    la $a0, message_out
+    syscall
 
+    # Stampa i numeri letti
+    li $t1, 0
+    la $t0, wVett
+
+ciclo_stampa:
+    lw $a0, ($t0)
+    li $v0, 1
+    syscall
+
+    la $a0, space
+    li $v0, 4
+    syscall
+
+    add $t0, $t0, 4
+    add $t1, $t1, 1
+
+    # Controlla se abbiamo stampato 5 numeri
+    bne $t1, DIM, ciclo_stampa
+
+    # Termina il programma
     li $v0, 10
     syscall
 
--- Esercitazione0/es7.asm
+++ Esercitazione0/es7.asm
@@ -1,13 +1,56 @@
-.data
-    
+DIM=5
 
-.text   
+.data
+    bVet: .space 5
+    bRes: .space 1
+    message_in: .asciiz "Inserire caratteri:\n"
+    message_out: .ascii "\nValore Minimo: "
+
+.text
 .globl main
 .ent main
 
 main:
-    # TODO: da completare
+    la $t0, bVet # puntatore a inizio del vettore
+    li $t1, 0 # contatore
+    la $a0, message_in # indirizzo della stringa
+    li $v0, 4 # system call –stampastringa
+    syscall
 
+ciclo_inserimenti:
+    li $v0, 12 # legge 1 char
+    syscall # system call (risultatoin $v0)
+
+    sb $v0, ($t0)
+    add $t1, $t1, 1
+    add $t0, $t0, 1
+
+    bne $t1, DIM, ciclo_inserimenti # itera 5 volte
+    
+    la $t0, bVet
+    li $t1, 0 # contatore
+    lb $t2, ($t0) # in $t2 memorizzo MIN iniziale
+
+ciclo_minimo:
+    lb $t3, ($t0)
+
+    bgt $t3, $t2, salta # salta se NON deve aggiornare MIN
+
+    lb $t2, ($t0) # aggiorna MIN
+
+salta: 
+    add $t1, $t1, 1
+    add $t0, $t0, 1
+
+    bne $t1, DIM, ciclo_minimo
+
+    la $a0, message_out
+    li $v0, 4
+    syscall
+
+    move $a0, $t2
+    li $v0, 11 # stampa 1 char
+    syscall
 
     li $v0, 10
     syscall
