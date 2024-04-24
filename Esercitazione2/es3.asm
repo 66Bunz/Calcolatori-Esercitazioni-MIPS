b56845d58b02d6090c777bf16bcff2baae995539
Bunz <66bunz@gmail.com>
2024-04-24 09:12:19 +0200

    commit

--- Esercitazione2/es3.asm
+++ Esercitazione2/es3.asm
@@ -1,6 +1,8 @@
+# Stampare a video le variabili in ordine crescente
+
 .data
-	w1: .word 123
-	w2: .word 456
+	w1: .word 456
+	w2: .word 132
 	w3: .word 789
 
 .text
@@ -12,8 +14,44 @@ main:
 	lw $t1, w2
 	lw $t2, w3
 
+	blt $t0, $t1, L1	# if w1 < w2, go to L1
+	move $t3, $t0		# else, swap w1 and w2
+	move $t0, $t1		# t3 = t0
+	move $t1, $t3		# t0 = t1
 
+L1:
+	blt $t1, $t2, L2	# if w2 < w3, go to L2
+	move $t3, $t1		# else, swap w2 and w3
+	move $t1, $t2		# t3 = t1
+	move $t2, $t3		# t1 = t2
 
+L2:
+	blt $t0, $t1, L3	# if w1 < w2, go to L3
+	move $t3, $t0		# else, swap w1 and w2
+	move $t0, $t1		# t3 = t0
+	move $t1, $t3		# t0 = t1
+
+L3:
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11			# print newline character
+	syscall
+
+	move $a0, $t1
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11			# print newline character
+	syscall
+
+	move $a0, $t2
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11			# print newline character
+	syscall
 
 	li $v0, 10
 	syscall
--- Esercitazione2/es4.asm
+++ Esercitazione2/es4.asm
@@ -1,11 +1,31 @@
 .data
+	num: .half 19791979
 
 .text
 .globl main
 .ent main
 
 main:
+	and $t3, $0, $0		# azzera campo risultato
+	and $t4, $0, $0		# azzera indice
 
+	lh $t0, num		# carica il numero
+	li $t1, 1		# dobbiamo contare i bit a 1, quindi carichiamo 1 per il confronto
+
+loop:
+	and $t2, $t0, $t1	# confronta il bit meno significativo con 1
+	beq $t2, $0, skip	# se il bit è 0, salta
+	addi $t3, $t3, 1	# incrementa di uno il campo risultato
+
+skip:
+	sll $t1, $t1, 1		# shifta a sinistra il bit di confronto
+	addi $t4, $t4, 1	# incrementa l'indice
+	bne $t4, 16, loop	# se non abbiamo finito, ripeti
+
+
+	move $a0, $t3		# carica il risultato in $a0
+	li $v0, 1		# carica il codice di syscall per la stampa di un intero
+	syscall
 
 	li $v0, 10
 	syscall
